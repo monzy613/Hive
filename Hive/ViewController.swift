32694ec84a3cd3822d25069770a481a4329deb18
monzy613 <monzy613@gmail.com>
2016-03-05 14:43:02 +0800

    Initial Commit

---
+++ Hive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hive
+//
+//  Created by 张逸 on 16/3/5.
+//  Copyright © 2016年 MonzyZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
